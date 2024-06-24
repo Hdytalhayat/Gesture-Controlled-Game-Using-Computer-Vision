783126703242fbbece6e4abc101efb95c6435dd6
Hidayatul Hayat <hayathidayat673@gmail.com>
2024-06-24 22:25:53 +0700

    Fix

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private Button dynoBtn, flappyBtn, aimlabBtn;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        dynoBtn.onClick.AddListener(OnClickDyno);
+        flappyBtn.onClick.AddListener(OnClickFlappy);
+        aimlabBtn.onClick.AddListener(OnClickAimlab);
+    }
+
+    private void OnClickDyno()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    private void OnClickFlappy()
+    {
+        SceneManager.LoadScene("mainGame");
+    }
+    private void OnClickAimlab()
+    {
+        SceneManager.LoadScene("NormalMode");
+    }
+}
