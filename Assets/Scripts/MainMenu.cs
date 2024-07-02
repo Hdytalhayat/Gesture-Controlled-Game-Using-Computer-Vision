cf14f0974168d46f23718ea092a858ae2acc7f2c
Hidayatul Hayat <87045924+Hdytalhayat@users.noreply.github.com>
2024-07-02 10:12:05 +0700

    .

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,22 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
 
-    public Button buttonDyno;
-    public Button buttonFlappy;
-    public Button buttonAim;
+    public Button[] buttons;
+    private int selectedButtonIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
-        buttonDyno.onClick.AddListener(DynoS);
-        buttonFlappy.onClick.AddListener(Flappy);
-        buttonAim.onClick.AddListener(Aims);
-        buttonDyno.Select();
+        SelectButton(selectedButtonIndex);
     }
 
     private void DynoS()
@@ -56,5 +51,31 @@ public class MainMenu : MonoBehaviour
         {
             Aims(); 
         }
+        // Mendeteksi input dari keyboard untuk mengubah pemilihan tombol
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            ChangeSelectedButton(-1); // Pilih tombol sebelumnya
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            ChangeSelectedButton(1); // Pilih tombol berikutnya
+        }
+
+    }
+    void ChangeSelectedButton(int direction)
+    {
+        // Menentukan indeks baru berdasarkan arah perubahan pemilihan (1 untuk ke bawah, -1 untuk ke atas)
+        selectedButtonIndex += direction;
+        // Pastikan indeks tetap dalam rentang yang valid
+        selectedButtonIndex = Mathf.Clamp(selectedButtonIndex, 0, buttons.Length - 1);
+
+        // Memilih tombol yang baru
+        SelectButton(selectedButtonIndex);
+    }
+
+    void SelectButton(int index)
+    {
+        // Mengatur focus pada tombol yang dipilih
+        EventSystem.current.SetSelectedGameObject(buttons[index].gameObject);
     }
 }
