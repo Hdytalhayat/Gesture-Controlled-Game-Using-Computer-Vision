32168ce3134336c612d75506782586fb2a163811
Hidayatul Hayat <hayathidayat673@gmail.com>
2024-07-04 23:39:39 +0700

    Add input sign

--- Assets/Game/ABC/AlphabetGame2.cs
+++ Assets/Game/ABC/AlphabetGame2.cs
@@ -10,11 +10,31 @@ public class AlphabetGame2 : MonoBehaviour
     public Text spawnIntervalText;
 
     private char currentLetter;
-    private int score;
+    private int score = 0;
     private bool canScore; // Flag to indicate if scoring is allowed
+    public GameObject udphandler;
 
+    float wait = 1f;
+    UDPReceive uDPReceive;
+    string data;
+    string[] points;
+    char CharInput;
+
+    private void DataHandle()
+    {
+        data = uDPReceive.data;
+        // Remove the first and last character
+        data = data.Remove(0, 1);
+        data = data.Remove(data.Length - 1, 1);
+        points = data.Split(", ");
+        CharInput = points[0][1];
+        // print(points[0]+""+ points[1]); // Print the first point for debugging
+    }
     void Start()
     {
+        uDPReceive = udphandler.GetComponent<UDPReceive>();
+        data = uDPReceive.data;
+
         score = 0;
         UpdateScore();
         StartCoroutine(SpawnLetter());
@@ -22,53 +42,47 @@ public class AlphabetGame2 : MonoBehaviour
 
     void Update()
     {
-        if (Input.anyKeyDown)
-        {
-            foreach (char c in Input.inputString)
-            {
-                if (char.IsLetter(c))
-                {
-                    OnLetterKeyPressed(char.ToUpper(c));
-                }
-            }
-        }
+        DataHandle();
+        Debug.Log(CharInput+""+currentLetter);
+        OnLetterKeyPressed(char.ToUpper(CharInput));
+        
     }
 
     IEnumerator SpawnLetter()
     {
-        
-
-        while (true)
+        while(true)
         {
             spawnIntervalText.gameObject.SetActive(true);
             spawnIntervalText.text = "Count = 10";
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(wait);
             spawnIntervalText.text = "Count = 9";
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(wait);
             spawnIntervalText.text = "Count = 8";
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(wait);
             spawnIntervalText.text = "Count = 7";
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(wait);
             spawnIntervalText.text = "Count = 6";
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(wait);
             spawnIntervalText.text = "Count = 5";
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(wait);
             spawnIntervalText.text = "Count = 4";
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(wait);
             spawnIntervalText.text = "Count = 3";
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(wait);
             spawnIntervalText.text = "Count = 2";
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(wait);
             spawnIntervalText.text = "Count = 1";
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(wait);
             spawnIntervalText.gameObject.SetActive(false);
+            
 
-            currentLetter = (char)('A' + Random.Range(0, 26));
+            currentLetter = (char)('A' + Random.Range(0, 13));
             letterText.text = currentLetter.ToString();
             canScore = true; // Reset scoring ability for the new letter
             letterText.color = Color.white;
-
         }
+
+
     }
 
     void OnLetterKeyPressed(char letter)
@@ -77,17 +91,13 @@ public class AlphabetGame2 : MonoBehaviour
         {
             if (letter == currentLetter)
             {
+                
                 score++;
                 letterText.color = Color.green;
-            }
-            else
-            {
-                score--;
-                letterText.color = Color.red;
+                UpdateScore();
+                canScore = false; // Disable scoring until the next letter is spawned
             }
 
-            UpdateScore();
-            canScore = false; // Disable scoring until the next letter is spawned
         }
     }
 
