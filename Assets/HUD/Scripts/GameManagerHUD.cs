fe3da09eb8d1286780d2cab08c76bc3d2dbfd809
Hidayatul Hayat <87045924+Hdytalhayat@users.noreply.github.com>
2024-07-11 10:05:09 +0700

    Add Opening

---
+++ Assets/HUD/Scripts/GameManagerHUD.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManagerHUD : MonoBehaviour
+{
+    [SerializeField]private TextMeshProUGUI textOpening;
+    [SerializeField]private Animator animator;
+    [SerializeField]private float transitionTime = 1f;
+    [SerializeField]private int indexs = 0;
+    [SerializeField]private string[] textOpenings = {"Welcome", "DLI Present", "NAMA GAME"};
+    private void Start()
+    {
+        animator = textOpening.gameObject.GetComponent<Animator>();
+        textOpening.text = textOpenings[indexs];
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(PlayFade());
+        }
+    }
+    IEnumerator PlayFade()
+    {
+        indexs++;
+        animator.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(transitionTime);
+        animator.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(transitionTime);
+        if(indexs < textOpenings.Length)
+        {
+            textOpening.text = textOpenings[indexs];
+        }
+        else
+        {
+            SceneManager.LoadScene("MainMenuUtama");
+        }
+    }
+}
